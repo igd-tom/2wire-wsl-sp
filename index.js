7dab904e54679a3b3ed5a93b5542eff85e35cbee
Tom <thomas.finch@internationalgasdetectors.com>
2020-12-15 13:58:37 +0000

    added tw driver support to web sockets

--- index.js
+++ index.js
@@ -29,10 +29,11 @@
 //     }
 // )
 
-
-
+const tw_driver = require('./tw_driver');
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+
+tw_driver.driver.init();
 
 var server = http.createServer(function (request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
@@ -52,18 +53,64 @@ wsServer = new WebSocketServer({
 
 
 wsServer.on('request', function (request) {
+
+
     const connection = request.accept(null, request.origin);
 
     connection.on('message', function (message) {
-        console.log('Received Message:', message.utf8Data);
+        try {
+            let reqJson = JSON.parse(message.utf8Data)
+            let maxTries = reqJson.maxTries;
+            let timeout = reqJson.timeout;
+            let pkt = new tw_driver.TwPacket();
+            pkt.addr = reqJson.addr;
+            pkt.cmd = reqJson.cmd;
+            pkt.d1 = reqJson.d1;
+            pkt.d2 = reqJson.d2;
+            pkt.d3 = reqJson.d3;
+            pkt.d4 = reqJson.d4;
 
-        let reqJson = JSON.parse(message.utf8Data)
-        console.log(reqJson.b);
+            let respData = {
+                twResCode: 0,
+                numTries: 0,
+                elapsedTime: 0,
+                payload: {
+                    addr: 0,
+                    cmd: 0,
+                    d1: 0,
+                    d2: 0,
+                    d4: 0,
+                    d4: 0,
+                }
+            }
 
 
-        connection.sendUTF({ "a": reqJson.b * 3 });
+            tw_driver.driver.write(pkt, maxTries, timeout).then(
+                res => {
+                    respData.twResCode = res.result;
+                    respData.numTries = res.numTries;
+                    respData.elapsedTime = res.timeElapsed;
+                    respData.payload.addr = res.packet.addr;
+                    respData.payload.cmd = res.packet.cmd;
+                    respData.payload.d1 = res.packet.d1;
+                    respData.payload.d2 = res.packet.d2;
+                    respData.payload.d3 = res.packet.d3;
+                    respData.payload.d4 = res.packet.d4;
+
+                    connection.sendUTF(JSON.stringify(respData));
+                }
+            )
+
+        }
+
+        catch (e) {
+            console.log(e)
+        }
+
     });
     connection.on('close', function (reasonCode, description) {
         console.log('Client has disconnected.');
     });
+
+
 });
